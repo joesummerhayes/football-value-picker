c62f79bd0a4e87b2b334c6d997264279c197c3c8
Joe Summerhayes <joesummerhayes09@hotmail.com>
2019-08-29 16:27:16 +0100

    added odd comparison and new colour scheme

--- src/components/fixtures/Fixtures.js
+++ src/components/fixtures/Fixtures.js
@@ -18,7 +18,7 @@ class Fixtures extends React.Component {
 
         const statsApiKey = '7af2d7e9641bd0322a09b5d94e4c03dd81da28e216f60929aa790d6236ed5e30';
         const fixtureRequest =
-        `https://apiv2.apifootball.com/?action=get_predictions&from=${fromDate}&to=${toDate}&league_id=148&APIkey=${statsApiKey}`
+        `https://apiv2.apifootball.com/?action=get_predictions&from=${fromDate}&to=${toDate}&league_id=149&APIkey=${statsApiKey}`
 
         Request.get(fixtureRequest).then((response) =>{
             const fixtureArray = response.body;
@@ -29,8 +29,9 @@ class Fixtures extends React.Component {
 
         const oddsApiKey = '2c6b7d182fa278280c13e5e5a562ea1a';
         const oddsRequest = `https://api.the-odds-api.com/v3/odds/?apiKey=${oddsApiKey}&sport=soccer_epl&region=uk&mkt=h2h`;
+        const oddsRequestChampionship = `https://api.the-odds-api.com/v3/odds/?apiKey=${oddsApiKey}&sport=soccer_efl_champ&region=uk&mkt=h2h`;
         
-        Request.get(oddsRequest).then((response) => {
+        Request.get(oddsRequestChampionship).then((response) => {
             const oddsArray = response.body;
             this.setState({
                 odds: oddsArray.data
