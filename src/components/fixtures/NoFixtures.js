e6f912e16fd64e51dca30d00e632d4a797cba9d0
Joe Summerhayes <joesummerhayes09@hotmail.com>
2019-10-11 14:22:33 +0100

    Added no fixture component and shows when no fixture Array length

---
+++ src/components/fixtures/NoFixtures.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import photo from '../../images/noFixtures.jpeg'
+import './NoFixtures.css';
+
+class NoFixtures extends React.Component {
+    render() {
+        return (
+            <div className="no-fixture-container">
+                <div>
+                    <img src={photo} style={{width: "-webkit-fill-available"}} />
+                </div>
+                <div className="no-fixture-content">
+                    Seems there are no fixtures this gameweek, buggar.
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NoFixtures;
